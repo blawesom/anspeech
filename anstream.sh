fef103afc008737fec768038efa54a9bacceadce
blawesom <blaplane@gmail.com>
2024-05-08 09:57:50 +0200

    2 digits filenaming scheme

--- anstream.sh
+++ anstream.sh
@@ -6,4 +6,4 @@ playlist_file="$1"
 # Set your desired segment duration (in seconds)
 segment_duration=600
 
-ffmpeg -i $playlist_file -c copy -bsf:a aac_adtstoasc -f segment -segment_time $segment_duration an-$(date +"%d-%m-%Y")_%d.mkv
+ffmpeg -i $playlist_file -c copy -bsf:a aac_adtstoasc -f segment -segment_time $segment_duration an-$(date +"%d-%m-%Y")_%01d.mkv
